9faa0ec7adc39da746724f82c912167a541936fd
John Kelly <mr.johnkelly@live.com>
2015-11-25 14:06:25 -0800

    Up to chapter 6 in the go programming book

---
+++ the_go_programming_language/github/issue.go
@@ -0,0 +1,28 @@
+//Package github provides a GO API for the Github Issue tracker.
+//See https://developer.github.com/v3/search/#search-issues.
+package github
+
+import "time"
+
+//IssuesURL is the github issue url.
+const IssuesURL = "https://api.github.com/search/issues"
+
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string    // in Markdown format
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
