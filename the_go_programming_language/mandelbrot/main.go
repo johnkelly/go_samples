6c37c1ffd9de98669ec8c6450f32fa31083997f8
John Kelly <jkelly@yuzu.com>
2015-12-11 23:00:03 -0800

    Complete chapter 1

--- the_go_programming_language/mandelbrot/main.go
+++ the_go_programming_language/mandelbrot/main.go
@@ -30,14 +30,14 @@ func main() {
 }
 
 func mandelbrot(z complex128) color.Color {
-	const iterations = 200
+	const iterations = 2000
 	const contrast = 15
 
 	var v complex128
-	for n := uint8(0); n < iterations; n++ {
+	for n := uint32(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			return color.Gray{255 - contrast*uint8(n)}
 		}
 	}
 	return color.Black
