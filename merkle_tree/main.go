2f75a07056a1cfbd4fe3190a3af4769a10263e25
John Kelly <john@johnkellycode.com>
2017-06-11 15:50:28 -0700

    Add Merkle Tree Implementation
    * WIP Need to add display to verify

---
+++ merkle_tree/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type Node struct {
+	left  *Node
+	right *Node
+	hash  string
+}
+
+type MerkleTree struct {
+	root *Node
+}
+
+func main() {
+	values := []string{"apple", "banana", "carrot", "date", "eggplant", "fennel", "goat", "habanero", "ice"}
+	hashes := generateHashes(values)
+	nodes := generateNodes(hashes)
+
+	tree := &MerkleTree{root: buildMerkleTree(nodes)}
+
+	fmt.Printf("Merkle Root:\t%v\n", tree.root.hash)
+	fmt.Printf("Merkle Left:\t%v\n", tree.root.left.hash)
+	fmt.Printf("Merkle Right:\t%v\n", tree.root.right.hash)
+
+	// tree.Display()
+}
+
+func buildMerkleTree(nodes []*Node) *Node {
+	for len(nodes) > 1 {
+		newNodes := []*Node{}
+
+		if len(nodes)%2 != 0 {
+			lastNode := nodes[len(nodes)-1]
+			node := &Node{hash: lastNode.hash}
+			nodes = append(nodes, node)
+		}
+		for i := 0; i < len(nodes); i = i + 2 {
+			newNode := &Node{
+				left:  nodes[i],
+				right: nodes[i+1],
+				hash:  combinedHash(nodes[i].hash, nodes[i+1].hash),
+			}
+
+			fmt.Printf("HashA: %v\n", nodes[i].hash)
+			fmt.Printf("HashB: %v\n\n", nodes[i+1].hash)
+			fmt.Printf("Combined Hash: %v\n\n", newNode.hash)
+
+			newNodes = append(newNodes, newNode)
+		}
+		nodes = newNodes
+	}
+	return nodes[0]
+}
+
+func combinedHash(hashA, hashB string) string {
+	combined := hashA + hashB
+	fmt.Printf("COMBINED: %v\n", combined)
+	hash := sha256.Sum256([]byte(combined))
+	return fmt.Sprintf("%X", hash)
+}
+
+func generateNodes(hashes []string) (nodes []*Node) {
+	for _, hash := range hashes {
+		nodes = append(nodes, &Node{hash: hash})
+	}
+	return nodes
+}
+
+func generateHashes(values []string) (hashes []string) {
+	for _, value := range values {
+		hash := sha256.Sum256([]byte(value))
+		hexDigest := fmt.Sprintf("%X", hash)
+		hashes = append(hashes, hexDigest)
+	}
+	return hashes
+}
